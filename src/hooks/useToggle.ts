b4f54d14339a1ec0dd4967055bc5c47cd832bf09
AhmadHddad <AhmadHddad@outlook.com>
2023-03-25 22:20:40 +0300

    adding docs

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,16 +1,16 @@
 import { useCallback, useState } from 'react';
 
+/**
+ * @description useState but with toggle flags, when update is called will "toggle" the value 
+ */
 export default function useToggle(
   initialValue?: boolean
 ): [boolean, (nextVal?: boolean) => void] {
   const [val, setVal] = useState(Boolean(initialValue));
 
-  const updateVal = useCallback(
-    (nextVal?: boolean) => {
-      setVal(prev => (typeof nextVal === 'boolean' ? nextVal : !prev));
-    },
-    []
-  );
+  const updateVal = useCallback((nextVal?: boolean) => {
+    setVal(prev => (typeof nextVal === 'boolean' ? nextVal : !prev));
+  }, []);
 
   return [val, updateVal];
 }
