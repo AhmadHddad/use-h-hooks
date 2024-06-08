cd7b1e1845b4f91842165b92702d549ffaf85e6a
AhmadHddad <AhmadHddad@outlook.com>
2024-06-09 01:23:21 +0300

    fix example + type for useObjectState

--- src/hooks/useObjectState.ts
+++ src/hooks/useObjectState.ts
@@ -3,8 +3,12 @@ import useMountedState from './useMountedState';
 
 /**
  *@description React hook for state as object, when u call setState({a:2}) => will only update the key a with the value 2
+ @example const [state, setState] = useObjectState({a:1, b:2})
+console.log(state.a) //1
+setState({a:3})
+setState(prev => ({...prev, a: prev.a + 3}))
  */
-export default function useObjectState<S = {}>(
+export default function useObjectState<S extends Record<string, any>>(
   initialState: S
 ): [S, Dispatch<SetStateAction<S>>] {
   const [state, setState] = useMountedState(initialState || {});
