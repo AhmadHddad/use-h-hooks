8d0732a860a5b95916262f631d0a7e2887500331
AhmadHddad <AhmadHddad@outlook.com>
2024-07-20 23:46:58 +0300

    useToggle

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -3,6 +3,8 @@ import useMountedState from './useMountedState';
 
 /**
  * @description useState but with toggle flags, when update is called will "toggle" the value 
+ * @example const [state, toggleState] = useToggle(false);
+ * toggleState(); // you can either pass a boolean or nothing it will toggle the previous state
  */
 export default function useToggle(
   initialValue?: boolean
